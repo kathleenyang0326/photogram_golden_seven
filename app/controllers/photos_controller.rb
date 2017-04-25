11bc82fbe95b31a3b9f5ecc661979fd8f37aa607
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-04-25 09:40:08 -0500

    show page

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,11 @@
+class PhotosController < ApplicationController
+def show
+
+id_number=params["the_id_number"]
+  p=Photo.find(id_number)
+  @the_caption=p.caption
+  @the_image_url=p.source
+  @created_at=p.created_at
+  render("show.html.erb")
+end
+end
