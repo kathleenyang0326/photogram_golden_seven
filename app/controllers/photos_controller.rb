0d6909dfbceb74c9c459e469410bd1f37b107a8b
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-04-29 15:49:04 -0500

    readmeupdated

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,56 @@
 class PhotosController < ApplicationController
+def new_form
+render("new_form.html.erb")
+end
+
+def edit_form
+  @photo=Photo.find(params[:the_id])
+render("photos/edit_form.html.erb")
+end
+
+def update_row
+  p=Photo.find(params[:the_id])
+  p.source=params[:the_source]
+  p.caption=params [:the_caption]
+redirect_to=("/photos")
+end
+
+
+
+def create_row
+# render("photos/create_row.html.erb")
+
+p=Photo.new
+p.source=params["the_image_url"]
+p.caption=params["the_caption"]
+p.save
+# render("photos/create_row.html.erb")
+redirect_to("/photos")
+end
+
+
+
 def show
 
 id_number=params["the_id_number"]
-  p=Photo.find(id_number)
-  @the_caption=p.caption
-  @the_image_url=p.source
-  @created_at=p.created_at
+  @photo=Photo.find(id_number)
+  @the_caption=@photo.caption
+  @the_image_url=@photo.source
+  @created_at=@photo.created_at
   render("show.html.erb")
 end
 
 def index
-  @all_photos=Photo.all
+  @all_photos=Photo.all.order({:created_at=>:desc})
   render("index.html.erb")
 end
+def update_row
+  p=Photo.find(params[:the_id])
+  p.source=params["the_source"]
+  p.caption=params["the_caption"]
+  p.save
+redirect_to("/photos/:the_id")
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+get("/create_photo",{:controller=>"photos",:action=>"create_row"})
+#UPDATE
+get("/photos/:the_id/edit",{:controller=>"photos",:action=>"edit_form"})
+get("/update_photos/:the_id",{:controller=>"photos",:action=>"update_row"})
+
+#CREATE
+get("/photos/new",{:controller=>"photos",:action=>"new_form"})
+
+#READ
   get("/photos",{:controller=>"photos",:action=>"index"})
 
 get("/photos/:the_id_number",{:controller=>"photos",:action=>"show"})
@@ -7,3 +16,6 @@ get("/photos/:the_id_number",{:controller=>"photos",:action=>"show"})
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+#DELETE
